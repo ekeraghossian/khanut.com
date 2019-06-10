1b8771b6ac90a3e9ca810bb8336bdf472b19cc54
Eskandar <eskandar_keraghossian@hotmail.com>
2019-06-10 08:10:25 +0200

    tomcat comp added

--- webshop-customer/src/main/java/com/khanut/customer/webshop/Application.java
+++ webshop-customer/src/main/java/com/khanut/customer/webshop/Application.java
@@ -2,13 +2,14 @@ package com.khanut.customer.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableEurekaClient
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
